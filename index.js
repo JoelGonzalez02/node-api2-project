6a2a3ede0590033dee3a2bab8866e696aac70d0f
Joel Gonzalez <espjoemusic@gmail.comgir config --global user.name JoelGonzalez02>
2020-07-12 19:04:06 -0700

    mvp completed

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const server = require('./data/server');
 
 const PORT = 4000;
 
+server.get('/', (req, res) => {
+    res.send(
+        `<h1>Welcome to the posts api!</h1>`
+    )
+})
+
 server.listen(PORT, () => {
     console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)
 })
